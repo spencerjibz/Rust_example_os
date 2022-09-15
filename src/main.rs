0c62c4012d62ce4d915eec8ad4f48aa1cad8c68d
Spencerjibz <=spencernajib2@gmail.com>
2022-09-15 19:32:44 +0100

    implemented Writer for Vga buffer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 //#![no_main]
 use core::panic::PanicInfo;
+mod vga_buffer;
 // panic handler
 #[panic_handler]
 
@@ -9,15 +10,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 // panic
-static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i,&byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize*2) = byte;
-            *vga_buffer.offset(i as isize*2+1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
  loop {}
 }
+
