acbe97b5ae8c6f0084f43fe9c30f1bae2e28c62b
Spencerjibz <=spencernajib2@gmail.com>
2022-09-28 11:17:07 +0100

    added test-framework and serial port tests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,74 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 //#![no_main]
 use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
 // panic handler
+#[cfg(not(test))]
 #[panic_handler]
 
-fn panic(_info: &PanicInfo) -> ! { 
-    loop{}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
+    loop {}
 }
-// panic
+// panic handler  in test mode;
+#[cfg(test)]
+#[panic_handler]
 
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!(" Error:{} \n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-   
     println!("Hello again");
-     println!( "some numbers: {},{}",42,1.337);
- loop {}
+    //panic!("some panic method");
+    #[cfg(test)]
+    test_main();
+    loop {}
 }
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    // exit qemu after running all tests
+    exit_qemu(QemuExitCode::Success);
+}
+
+// trivial assertion
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion....");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+/* ------------------------------------------------------------- --------------------------
+
+    HANLDING QEMU EXIT FOR TESTING
+-----------------------------------------------------------------------------------------------*/
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    //
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(code as u32);
+    }
+}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
- use core::fmt::{self,Write,Result};
+use core::fmt::{self, Result, Write};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -22,131 +22,124 @@ pub enum Color {
     White = 15,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq,Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
- struct ColorCode(u8);
+struct ColorCode(u8);
 
- impl ColorCode {
-    fn new(foreground: Color,background: Color) -> Self {
-               Self((background as u8)<<4|(foreground as u8))
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> Self {
+        Self((background as u8) << 4 | (foreground as u8))
     }
- }
+}
 
- #[derive(Debug, Clone, Copy, PartialEq,Eq)]
- #[repr(C)]
- struct ScreenChar{
-    ascii_character:u8,
-    color_code:ColorCode
- }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
 
- const BUFFER_HEIGHT: usize = 25;
- const BUFFER_WIDTH: usize = 80;
- use volatile::Volatile;
- #[repr(transparent)]
- struct Buffer{
-   chars:[[Volatile<ScreenChar>;BUFFER_WIDTH];BUFFER_WIDTH],
- }
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+use volatile::Volatile;
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_WIDTH],
+}
 
- // Writer 
- pub struct Writer { 
-   column_position:usize,
-   color_code:ColorCode,
-   buffer:&'static mut Buffer,
- }
+// Writer
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
 
- impl Writer {
-   pub fn write_btye(&mut self,byte:u8) {
-      match byte {
-         b'\n' => self.new_line(),
-         byte => {
-            if self.column_position >= BUFFER_WIDTH {
-               self.new_line();
+impl Writer {
+    pub fn write_btye(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
             }
-            let row = BUFFER_HEIGHT-1;
-             let col = self.column_position;
-             let color_code = self.color_code;
-             self.buffer.chars[row][col].write(ScreenChar{
-               ascii_character:byte,
-               color_code
-             });
-             self.column_position += 1;
-         }
-      }
-   }
-   // method to move every character one line up (top line gets deleted) and start at the beginning of the last line again. 
-    fn new_line(&mut self) {
-      
-    for row in 1..BUFFER_HEIGHT{
-      for col in 0..BUFFER_WIDTH {
-         // read each character on each row
-          let character = self.buffer.chars[row][col].read();
-
-          self.buffer.chars[row-1][col].write(character); // remove new character to top row
-      }
+        }
     }
-    self.clear_row(BUFFER_HEIGHT-1);
-    self.column_position = 0;
-   
-   /*TODO */
-   
+    // method to move every character one line up (top line gets deleted) and start at the beginning of the last line again.
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                // read each character on each row
+                let character = self.buffer.chars[row][col].read();
 
+                self.buffer.chars[row - 1][col].write(character); // remove new character to top row
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+
+        /*TODO */
+    }
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ', //
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+    // read strings
 }
-fn clear_row(&mut self,row:usize) {
-    let blank = ScreenChar{
-      ascii_character:b' ', //
-   color_code:self.color_code,
-    };
 
-     for col in 0..BUFFER_WIDTH{
-      self.buffer.chars[row][col].write(blank);
-     }
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or new line
+                0x20..=0x7e | b'\n' => self.write_btye(byte),
+                _ => self.write_btye(0xfe),
+            }
+        }
+    }
 }
- // read strings
-    
-
- }
-
- impl Writer {
-   pub fn write_string(&mut self,s:&str) { 
-      for byte in s.bytes(){ 
-         match byte {
-            // printable ASCII byte or new line
-            0x20..=0x7e| b'\n' => self.write_btye(byte),
-            _ => self.write_btye(0xfe)
-         }
-      }
-   }
- }
 // Global Writer interface
 use lazy_static::lazy_static;
 use spin::Mutex;
-lazy_static!{
-pub static ref WRITER: Mutex<Writer>=  Mutex::new(Writer{
-      column_position:0,
-      color_code:ColorCode::new(Color::Yellow, Color::Black),
-      buffer: unsafe { &mut *(0xb8000 as *mut Buffer)},
-   });
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
 }
- 
+
 // implement write and writeln! macro;
 
- impl Write for Writer {
-   fn write_str(&mut self,s:&str) -> Result{
-      self.write_string(s);
-      Ok(())
-   }
- }
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
- /* --------------------------------------------------------------------------------------
- 
+/* --------------------------------------------------------------------------------------
+
                Print and Println Macros
 ----------------------------------------------------------------------------
- 
-*/ 
-  
 
+*/
 
- #[macro_export]
+#[macro_export]
 macro_rules! print {
  ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
  }
@@ -159,6 +152,5 @@ macro_rules! println {
 
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
- 
     WRITER.lock().write_fmt(args).unwrap();
 }
